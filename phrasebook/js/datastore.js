ed0c2de0090b2c160ef057bf502fd8ff9a763741
glottotopia <sebastian.nordhoff@glottotopia.de>
2015-09-11 13:06:12 +0200

    add domains

--- phrasebook/js/datastore.js
+++ phrasebook/js/datastore.js
@@ -1,178 +1,191 @@
 function vardata(){
   return [   
+    {
+        "source" :       "здраво",
+        "target":   "Hallo",
+        "phonetic":     "хало",
+        "domain": "greetings",
+    },
+    {
+        "source":       "Добар дан",
+        "target":   "Guten Morgen",
+        "phonetic":     "гутен морген",
+        "domain": "greetings",
+    }
+,
 {
 "target":	"guten Abend",
 "source":	"добро вече",
 "phonetic":	"гутен абент ",
-"domain":""
+"domain":"politeness"
 }
 ,
 {
 "target":	"auf Wiedersehen",
 "source":	"Довиђења",
 "phonetic":	"авуф видашен ",
-"domain":""
+"domain":"politeness"
 }
 ,
 {
 "target":	"Ich heiße...",
 "source":	"Ја се зовем",
 "phonetic":	"их хаише ",
-"domain":""
+"domain":"presentation"
 }
 ,
 {
 "target":	"Ich komme aus...",
 "source":	"Ја сам из",
 "phonetic":	"их коме аус ",
-"domain":""
+"domain":"presentation"
 }
 ,
 {
 "target":	"Entschuldigung",
 "source":	"Опростите",
 "phonetic":	"енчулдигунг",
-"domain":""
+"domain":"politeness"
 }
 ,
 {
 "target":	"bitte",
 "source":	"Молим",
 "phonetic":	"бите",
-"domain":""
+"domain":"politeness"
 }
 ,
 {
 "target":	"danke",
 "source":	"Хвала",
 "phonetic":	"данке",
-"domain":""
+"domain":"politeness"
 }
 ,
 {
 "target":	"das ist… mein Mann",
 "source":	"Oво је мој муж",
 "phonetic":	"даш ист маин ман  ",
-"domain":""
+"domain":"presentation"
 }
 ,
 {
 "target":	"meine Frau",
 "source":	"Oво је моја жена",
 "phonetic":	"маине фрау ",
-"domain":""
+"domain":"kin"
 }
 ,
 {
 "target":	"meine Kinder",
 "source":	"Oво cu mојa деца",
 "phonetic":	"маине кинда ",
-"domain":""
+"domain":"kin"
 }
 ,
 {
 "target":	"meine Tochter",
 "source":	"Oво је моја ћерка",
 "phonetic":	"маине тохта ",
-"domain":""
+"domain":"kin"
 }
 ,
 {
 "target":	"mein Sohn",
 "source":	"Oво је мој син",
 "phonetic":	"маин зон  ",
-"domain":""
+"domain":"kin"
 }
 ,
 {
 "target":	"mein Baby",
 "source":	"Моја беба",
 "phonetic":	"маин беби  ",
-"domain":""
+"domain":"kin"
 }
 ,
 {
 "target":	"Ich habe Hunger.",
 "source":	"Гладна сам",
 "phonetic":	"ис хабе хунга ",
-"domain":""
+"domain":"sensations"
 }
 ,
 {
 "target":	"Ich habe Durst.",
 "source":	"Жедна сам",
 "phonetic":	"иx  хабе дурст ",
-"domain":""
+"domain":"sensations"
 }
 ,
 {
 "target":	"Ist das Essen Halal?",
 "source":	"Да ли је oвa храна халал?",
 "phonetic":	"ист даш есен халал ",
-"domain":""
+"domain":"food"
 }
 ,
 {
 "target":	"Ich bin krank.",
 "source":	"Болеснa сам",
 "phonetic":	" иx бин кранк ",
-"domain":""
+"domain":"medical"
 }
 ,
 {
 "target":	"[mein Kind] hat Hunger",
 "source":	"Mоје дете је гладно",
 "phonetic":	"хат хунга ",
-"domain":""
+"domain":"sensations"
 }
 ,
 {
 "target":	"[mein Kind] ist krank",
 "source":	"моје дете је болесно",
 "phonetic":	"ист кранк ",
-"domain":""
+"domain":"medical"
 }
 ,
 {
 "target":	"Wo ist...",
 "source":	"где je",
 "phonetic":	"во ист ",
-"domain":""
+"domain":"questions"
 }
 ,
 {
 "target":	"Sprechen Sie…?",
 "source":	"Говорите ли?",
 "phonetic":	"шпрехен зи ",
-"domain":""
+"domain":"communication"
 }
 ,
 {
 "target":	"Können Sie das für mich aufschreiben ?",
 "source":	"Можете ли ми то написати?",
 "phonetic":	"кенен си даш фир мис ауфшраибен ",
-"domain":""
+"domain":"communication"
 }
 ,
 {
 "target":	"Ich verstehe das nicht.",
 "source":	"Не разумем",
 "phonetic":	"иx  фаште даш нихт ",
-"domain":""
+"domain":"communication"
 }
 ,
 {
 "target":	"Ich spreche nicht so gut ...",
 "source":	"Не знам (добро) ...",
 "phonetic":	"иx  шпрехе никт со гут ...",
-"domain":""
+"domain":"communication"
 }
 ,
 {
 "target":	"Ich brauche einen Arzt.",
 "source":	"Треба ми доктор",
 "phonetic":	"их браухе аинен арцт ",
-"domain":""
+"domain":"medical"
 }
 ,
 {
@@ -186,7 +199,7 @@ function vardata(){
 "target":	"Ich weiß nicht",
 "source":	"не знам",
 "phonetic":	"их ваиш никхт ",
-"domain":""
+"domain":"communication"
 }
 ,
 {
@@ -200,70 +213,70 @@ function vardata(){
 "target":	"Ich habe Kopfschmerzen.",
 "source":	"Боли ме глава",
 "phonetic":	"их хабе копф шмеацен",
-"domain":""
+"domain":"medical"
 }
 ,
 {
 "target":	"Ich habe Bauchschmerzen.",
 "source":	"Боли ме трбух (стомак)",
 "phonetic":	"их хабе бауx шмеацен",
-"domain":""
+"domain":"medical"
 }
 ,
 {
 "target":	"Ich fühle mich nicht gut.",
 "source":	"Jа се не осећам добро",
 "phonetic":	"их филе мих ниxт гут ",
-"domain":""
+"domain":"medical"
 }
 ,
 {
 "target":	"Wo ist die Toilette?  / Wo ist das Badezimmer?",
 "source":	"где је тоалет?",
 "phonetic":	"во ист ди толете / во ист дас бадецима ",
-"domain":""
+"domain":"needs"
 }
 ,
 {
 "target":	"Ich bin allergisch gegen…",
 "source":	"имам алергију на...",
 "phonetic":	"их бин алеагиш",
-"domain":""
+"domain":"medical"
 }
 ,
 {
 "target":	"Ich friere. /  Mir ist kalt.",
 "source":	"хладно mi je",
 "phonetic":	"их фрире / мир ист калт ",
-"domain":""
+"domain":"sensations"
 }
 ,
 {
 "target":	"Ich freue mich / ich bin glücklich",
 "source":	"сретна сам  ",
 "phonetic":	"их фроие мих ",
-"domain":""
+"domain":"sensations"
 }
 ,
 {
 "target":	"Ich habe Angst",
 "source":	"Бојим се (Страх ме је)",
 "phonetic":	"их хабе ангст ",
-"domain":""
+"domain":"sensations"
 }
 ,
 {
 "target":	"Ich bin traurig.",
 "source":	"Тужан сам",
 "phonetic":	"их бин траурик ",
-"domain":""
+"domain":"sensations"
 }
 ,
 {
 "target":	"Ich mag dich.",
 "source":	"Свиђате ми се",
 "phonetic":	"их мак дих ",
-"domain":""
+"domain":"sensations"
 }
 ,
 {
@@ -277,34 +290,34 @@ function vardata(){
 "target":	"Sprechen Sie Englisch?",
 "source":	"Говорите ли енглески?",
 "phonetic":	"шпрехен си енглиш ",
-"domain":""
+"domain":"communication"
 }
 ,
 {
 "target":	"Wie heißen Sie?",
 "source":	"Како се зовете?",
 "phonetic":	"ви хаишен си ",
-"domain":""
+"domain":"presentation"
 }
 ,
 {
 "target":	"Ich habe Durst.",
 "source":	"Жедна сам",
 "phonetic":	"их хабе дурст ",
-"domain":""
+"domain":"sensations"
 }
 ,
 {
 "target":	"Das ist mein Bruder.",
 "source":	"Oво је мој брат",
 "phonetic":	"даш ист маин бруда ",
-"domain":""
+"domain":"presentation"
 },
 {
 "target":	"Das ist meine Schwester.",
 "source":	"Oво је моја сестра",
 "phonetic":	"даш ист маине швеста ",
-"domain":""
+"domain":"presentation"
 }
 
 ]
